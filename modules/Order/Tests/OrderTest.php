98f122e59eb45fb10cbe5332df19b47cf128ea5a
Samuel Esplana <samesplana_dev@my.pisopay.com.ph>
2024-02-28 14:08:42 +0800

    E5 - Automated Tests

---
+++ modules/Order/Tests/OrderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Order\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class OrderTest extends TestCase
+{
+    public function test_it_creates_an_order(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ modules/Product/Tests/ProductTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Product\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_it_can_create_product()
+    {
+        $this->assertTrue(true);
+    }
+}
