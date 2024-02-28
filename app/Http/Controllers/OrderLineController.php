1b13225748c095694704b102e29db82479a2a6c6
Samuel Esplana <samesplana_dev@my.pisopay.com.ph>
2024-02-28 11:02:49 +0800

    First Commit

---
+++ app/Http/Controllers/OrderLineController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreOrderLineRequest;
+use App\Http\Requests\UpdateOrderLineRequest;
+use Modules\Order\Models\OrderLine;
+
+class OrderLineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrderLineRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderLine $orderLine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderLine $orderLine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOrderLineRequest $request, OrderLine $orderLine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderLine $orderLine)
+    {
+        //
+    }
+}
